059d857a2185e8c6fcda4ab1e158768b90d51038
xuyanan <xuyanan@315i.com>
2025-12-31 09:08:52 +0800

    config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Java', link: '/Java/' },
+      { text: 'Java', link: '/Java' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
