8cb04fdddbef706928d7cf066ade6d9bb50398f4
xuyanan <xuyanan@315i.com>
2025-12-31 09:53:47 +0800

    config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -12,26 +12,32 @@ export default defineConfig({
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      },
-      {
-        text: 'Java',
-        items: [
-          { text: '经典问题', link: '/Java/经典问题' },
-          { text: '面试', items: [
-              { text: '经典问题', link: '/Java/经典问题' }
-            ]
-          },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    sidebar: {
+      // 当用户位于 `guide` 目录时，会显示此侧边栏
+      '/Java/': [
+        {
+          text: 'Java',
+          items: [
+            { text: '经典问题', link: '/Java/经典问题' },
+            { text: '面试', items: [
+                { text: '经典问题', link: '/Java/经典问题' }
+              ]
+            }
+          ]
+        }
+      ],
+
+      // 当用户位于 `config` 目录时，会显示此侧边栏
+      '/markdown-examples/': [
+        {
+          text: 'Examples',
+          items: [
+            { text: 'Markdown Examples', link: '/markdown-examples' },
+            { text: 'Runtime API Examples', link: '/api-examples' }
+          ]
+        }
+      ]
+    }
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
